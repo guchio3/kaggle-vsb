4785f96cbdbb8505c88178ff6025f072bd6e0d72
naoya taguchi <astt.hwhw@gmail.com>
2019-01-16 02:50:52 +0900

    e007 added threshold features

--- tools/features/hp_dn_features.py
+++ tools/features/hp_dn_features.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pyarrow.parquet as pq
+import scipy
 
 from ..utils.general_utils import dec_timer, sel_log
 
@@ -16,8 +17,8 @@ def e002_hp_dn_basic(df):
     return features
 
 
-def e006_hp_dn_percentiles(df):
-    df = df.astype('float64')
+def e006_hp_dn_abs_percentiles(df):
+    df = df.abs().astype('float64')
     features = pd.DataFrame()
     features['perc1'] = np.percentile(df, 1, axis=0)
     features['perc5'] = np.percentile(df, 5, axis=0)
@@ -36,6 +37,50 @@ def e006_hp_dn_percentiles(df):
     return features
 
 
+def e007_hp_dn_abs_thresh_overs(df):
+    df = df.abs().astype('float64')
+    features = pd.DataFrame()
+    # threshold 1
+    th1_df = df[df > 1]
+    features['th1_count'] = th1_df.count()
+    features['th1_time_std'] = [
+        np.std(th1_df[col].dropna().index) for col in th1_df.columns]
+    features['th1_time_skew'] = [scipy.stats.skew(
+        th1_df[col].dropna()) for col in th1_df.columns]
+    features['th1_time_kurt'] = [scipy.stats.kurtosis(
+        th1_df[col].dropna()) for col in th1_df.columns]
+    # threshold 3
+    th3_df = df[df > 3]
+    features['th3_count'] = th3_df.count()
+    features['th3_time_std'] = [
+        np.std(th3_df[col].dropna().index) for col in th3_df.columns]
+    features['th3_time_skew'] = [scipy.stats.skew(
+        th3_df[col].dropna()) for col in th3_df.columns]
+    features['th3_time_kurt'] = [scipy.stats.kurtosis(
+        th3_df[col].dropna()) for col in th3_df.columns]
+    # threshold 5
+    th5_df = df[df > 5]
+    features['th5_count'] = th5_df.count()
+    features['th5_time_std'] = [
+        np.std(th5_df[col].dropna().index) for col in th5_df.columns]
+    features['th5_time_skew'] = [scipy.stats.skew(
+        th5_df[col].dropna()) for col in th5_df.columns]
+    features['th5_time_kurt'] = [scipy.stats.kurtosis(
+        th5_df[col].dropna()) for col in th5_df.columns]
+    # threshold 10
+    th10_df = df[df > 10]
+    features['th10_count'] = th5_df.count()
+    features['th10_time_std'] = [
+        np.std(th10_df[col].dropna().index) for col in th10_df.columns]
+    features['th10_time_skew'] = [scipy.stats.skew(
+        th10_df[col].dropna()) for col in th10_df.columns]
+    features['th10_time_kurt'] = [scipy.stats.kurtosis(
+        th10_df[col].dropna()) for col in th10_df.columns]
+    features = features.add_prefix(
+        'e007_hp_dn_abs_thresh_overs_').reset_index(drop=True)
+    return features
+
+
 def _hp_dn_features(df, exp_ids):
     _features = []
     _features.append(pd.DataFrame(df.columns,
@@ -43,6 +88,10 @@ def _hp_dn_features(df, exp_ids):
                                   dtype=int))
     if 'e002' in exp_ids:
         _features.append(e002_hp_dn_basic(df))
+    if 'e006' in exp_ids:
+        _features.append(e006_hp_dn_abs_percentiles(df))
+    if 'e007' in exp_ids:
+        _features.append(e007_hp_dn_abs_thresh_overs(df))
     features = pd.concat(_features, axis=1)
     return features
 
@@ -52,6 +101,7 @@ def _load_hp_dn_features_src(exp_ids, test, series_df, meta_df, logger):
     target_ids = [
         'e002',
         'e006',
+        'e007',
     ]
     if len(set(target_ids) & set(exp_ids)) < 1:
         sel_log(f'''
