ce3134044643358764cbf27e24aa39d2f431fb65
naoya taguchi <astt.hwhw@gmail.com>
2019-01-17 11:58:16 +0900

    e010 submit

--- lgb_train.py
+++ lgb_train.py
@@ -7,7 +7,7 @@ from logging import getLogger
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import GroupShuffleSplit, GroupKFold
+from sklearn.model_selection import GroupKFold, GroupShuffleSplit
 from tqdm import tqdm
 
 import tools.models.my_lightgbm as mlgb
@@ -65,6 +65,7 @@ def train(args, logger):
         if args.gen_cached_features:
             features_df.to_pickle(
                 './inputs/train/cached_featurse.pkl.gz', compression='gzip')
+    features = features_df.columns
 
     # -- Data resampling
     # Stock original data for validation
@@ -82,6 +83,9 @@ def train(args, logger):
     # gss = GroupShuffleSplit(configs['train']['fold_num'], random_state=71)
     # folds = gss.split(features_df, target, groups=id_measurement)
     folds, pred_folds = tee(folds)
+    if configs['train']['label_train']:
+        folds, folds_2 = tee(folds)
+        folds, pred_folds_2 = tee(folds)
 
     # -- Make training dataset
     train_set = mlgb.Dataset(features_df.values, target.values)
@@ -134,61 +138,6 @@ def train(args, logger):
 
     sel_log(f'MCC_mean: {np.mean(scores)}, MCC_std: {np.std(scores)}', logger)
 
-    # -- Retrainig using the preds
-    if configs['train']['label_train']:
-        # -- Make training dataset
-        y_pres_df = y_preds_features(oofs, val_idxes)
-        features_df = pd.concat([features_df, y_preds_df], axis=1)
-        train_set = mlgb.Dataset(features_df.values, target.values)
-    
-        # -- CV
-        # Set params
-        PARAMS = configs['lgbm_params']
-        PARAMS['nthread'] = args.nthread
-    
-        sel_log('RETRAINED -- start training ...', None)
-        hist, cv_model = mlgb.cv(
-            params=PARAMS,
-            num_boost_round=10000,
-            folds=folds,
-            train_set=train_set,
-            verbose_eval=50,
-            early_stopping_rounds=200,
-            metrics='auc',
-            # feval=lgb_MCC,
-            callbacks=[log_evaluation(logger, period=50)],
-        )
-    
-        # -- Prediction
-        sel_log('RETRAINED -- predicting ...', logger)
-        oofs = []
-        y_trues = []
-        val_idxes = []
-        scores = []
-        fold_importance_dict = {}
-        for i, idxes in tqdm(list(enumerate(pred_folds))):
-            trn_idx, val_idx = idxes
-            booster = cv_model.boosters[i]
-    
-            # Get and store oof and y_true
-            y_pred = booster.predict(features_df.values[val_idx])
-            y_true = target.values[val_idx]
-            oofs.append(y_pred)
-            y_trues.append(y_true)
-            val_idxes.append(val_idx)
-    
-            # Calc MCC using thresh of 0.5
-            MCC = calc_MCC(y_true, y_pred, 0.5)
-            scores.append(MCC)
-    
-            # Save importance info
-            fold_importance_df = pd.DataFrame()
-            fold_importance_df['split'] = booster.feature_importance('split')
-            fold_importance_df['gain'] = booster.feature_importance('gain')
-            fold_importance_dict[i] = fold_importance_df
-    
-        sel_log(f'RETRAINED -- MCC_mean: {np.mean(scores)}, MCC_std: {np.std(scores)}', logger)
-    
     # Calc best MCC
     sel_log('calculating the best MCC ...', None)
     y_true = np.concatenate(y_trues, axis=0)
@@ -204,13 +153,94 @@ def train(args, logger):
         pickle.dump([val_idxes, oofs], fout)
 
     # Save importances
-    save_importance(configs['features'], fold_importance_dict,
+    # save_importance(configs['features'], fold_importance_dict,
+    save_importance(features, fold_importance_dict,
                     './importances/' + filename_base + '_importances')
 
     # Save trained models
     with open(
             './trained_models/' + filename_base + '_models.pkl', 'wb') as fout:
         pickle.dump(cv_model, fout)
+
+    # -- Retrainig using the preds
+    if configs['train']['label_train']:
+        # -- Make training dataset
+        y_preds_df = y_preds_features(oofs, val_idxes)
+        features_df_2 = features_df
+        features_df_2 = pd.concat([features_df_2, y_preds_df], axis=1)
+        features_2 = features_df_2.columns
+        train_set_2 = mlgb.Dataset(features_df_2.values, target.values)
+
+        # -- CV
+        sel_log('RETRAINED -- start training ...', None)
+        hist_2, cv_model_2 = mlgb.cv(
+            params=PARAMS,
+            num_boost_round=10000,
+            folds=folds_2,
+            train_set=train_set_2,
+            verbose_eval=50,
+            early_stopping_rounds=200,
+            metrics='auc',
+            # feval=lgb_MCC,
+            callbacks=[log_evaluation(logger, period=50)],
+        )
+
+        # -- Prediction
+        sel_log('RETRAINED -- predicting ...', logger)
+        oofs_2 = []
+        y_trues_2 = []
+        val_idxes_2 = []
+        scores_2 = []
+        fold_importance_dict_2 = {}
+        for i, idxes in tqdm(list(enumerate(pred_folds_2))):
+            trn_idx, val_idx = idxes
+            booster = cv_model_2.boosters[i]
+
+            # Get and store oof and y_true
+            y_pred = booster.predict(features_df_2.values[val_idx])
+            y_true = target.values[val_idx]
+            oofs_2.append(y_pred)
+            y_trues_2.append(y_true)
+            val_idxes_2.append(val_idx)
+
+            # Calc MCC using thresh of 0.5
+            MCC = calc_MCC(y_true, y_pred, 0.5)
+            scores_2.append(MCC)
+
+            # Save importance info
+            fold_importance_df = pd.DataFrame()
+            fold_importance_df['split'] = booster.feature_importance('split')
+            fold_importance_df['gain'] = booster.feature_importance('gain')
+            fold_importance_dict_2[i] = fold_importance_df
+
+        sel_log(
+            f'RETRAINED -- MCC_mean: {np.mean(scores_2)}, MCC_std: {np.std(scores_2)}',
+            logger)
+
+        # Calc best MCC
+        sel_log('RETRAINED -- calculating the best MCC ...', None)
+        y_true_2 = np.concatenate(y_trues_2, axis=0)
+        y_pred_2 = np.concatenate(oofs_2, axis=0)
+        best_MCC_2, best_thresh_2 = calc_best_MCC(y_true_2, y_pred_2, bins=30)
+        sel_log(
+            f'RETRAINED -- best_MCC: {best_MCC_2}, best_thresh: {best_thresh_2}',
+            logger)
+
+        # -- Post processings
+        filename_base = f'{args.exp_ids[0]}_{exp_time}_{best_MCC_2:.4}_{best_thresh_2:.3}'
+
+        # Save oofs
+        with open('./oofs/' + filename_base + '_oofs_retrained.pkl', 'wb') as fout:
+            pickle.dump([val_idxes_2, oofs_2], fout)
+
+        # Save importances
+        save_importance(features_2, fold_importance_dict_2,
+                        './importances/' + filename_base + '_importances_retrained')
+
+        # Save trained models
+        with open(
+                './trained_models/' + filename_base + '_models_retrained.pkl', 'wb') as fout:
+            pickle.dump(cv_model_2, fout)
 
     # --- Make submission file
     if args.test:
@@ -226,11 +256,23 @@ def train(args, logger):
         preds = []
         for booster in tqdm(cv_model.boosters):
             preds.append(booster.predict(test_features_df.values))
-
-        # -- Make submission file
         sub_values = np.mean(preds, axis=0)
         target_values = (sub_values > best_thresh).astype(np.int32)
 
+        if configs['train']['label_train']:
+            # -- Use retrained info
+            test_y_preds_df = y_preds_features(
+                [sub_values], [np.arange(len(sub_values))])
+            test_features_df = pd.concat(
+                [test_features_df, test_y_preds_df], axis=1)
+            sel_log('RETRAINED -- predicting ...', None)
+            preds = []
+            for booster in tqdm(cv_model_2.boosters):
+                preds.append(booster.predict(test_features_df.values))
+            sub_values = np.mean(preds, axis=0)
+            target_values = (sub_values > best_thresh_2).astype(np.int32)
+
+        # -- Make submission file
         sel_log(f'loading sample submission file ...', None)
         sub_df = pd.read_csv('./inputs/origin/sample_submission.csv')
         sub_df.target = target_values
